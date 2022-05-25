fcd5680f5602b83f8071d756b15b2ffe2e0346b5
Mehran Falahati <mehran_falahati@yahoo.com>
2022-05-25 10:17:43 +1000

    sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,18 @@ class UsersController < ApplicationController
         render json: @user, status: :created
     end
 
+    def login
+        @user = User.find_by(username: params[:user][:username])
+
+        if @user && @user.authenticate(params[:user][:password])
+          @token = JWT.encode({user_id: @user.id}, Rails.application.secrets.secret_key_base[0])
+    
+          render json: {user: @user, token: @token}
+        else
+          render json: {error: "Invalid Credentials"}, status: :unauthorized
+        end
+    end
+
     private
     def user_params
         params.require(:user).permit(:username, :password, :first_name, :last_name)
